60b02b0f7ced7828007bbd2201a41956ae34ce82
Zalfapontianak <102153102+Zalfapontianak@users.noreply.github.com>
2022-06-03 20:39:58 +0700

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -545,26 +545,26 @@ let str = `♡   ❍ㅤ  ⎙ㅤ ⌲
 
 
   ⬣━〔Powered By Rozi〕━⬣
-©️ zifabotz`
+© zifabotz`
 let name = await conn.getName(m.sender)
 
 let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
 
   sumberImg = await (await fetch(menu)).buffer()
-  image = (await conn.prepareMessage('6287773973818@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
+  image = (await conn.prepareMessage('6285826114932@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
   res = await conn.prepareMessageFromContent(m.chat, {
     "productMessage": {
       "product": {
         "productImage": image,
         "productId": "4938174216214248",
         "title": "𝚃𝙰𝙼𝙿𝙸𝙻𝙰𝙽 𝙼𝙴𝙽𝚄 𝚉𝙸𝙵𝙰𝙱𝙾𝚃𝚉",
-        "description": '\n' + watermark + '\n' + str,
+        "description": '\n' + wm + '\n' + str,
         "retailerId": "Thanks For zifabotz !",
         "url": "▌│█║▌║▌║║▌║▌║█│▌",
         "descriptionCount": "999999999",
         "productImageCount": "1",
       },
-      "businessOwnerJid": "6287773973818@s.whatsapp.net",
+      "businessOwnerJid": "6285828764046@s.whatsapp.net",
       "contextInfo": {
         "forwardingScore": 9999,
         "isForwarded": true
@@ -574,9 +574,9 @@ let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ?
     { quoted: fkon })
   conn.relayWAMessage(res)
 }
-handler.help = ['menu2', 'help2', '2']
+handler.help = ['menu']
 handler.tags = ['info']
-handler.command = /^(menu2|help2|/2)$/
+handler.command = /^(menu)$/
 
 module.exports = handler
 
