3dba45e64374d87127b788f45c1acbf480bbe4d8
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-06-03 23:43:19 +0700

    Update nangis.js

--- plugins/nangis.js
+++ plugins/nangis.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
   Status : *_sad_* 
 *📮: KENAPA NANGIS!? ADA AKU DI SINI KOK SETIA MENEMANI KAMU*
 `.trim()
-conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'JANGAN SEDIH')).buffer(), ext, 'sedih ya', 'COMMAND', '.menu2', 'RULES', '.rules', m)
+conn.send2ButtonImg(m.chat, await (await fetch(fla + 'JANGAN SEDIH')).buffer(), ext, 'sedih ya', 'COMMAND', '.menu2', 'RULES', '.rules', m)
 
 }
 handler.customPrefix = /^(😭|😔|🙃|🙂|🥺|😟|😥|😢|☹️|😓|😞|😖|😣|😩|😫)$/i
