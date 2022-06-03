64f221772b365b23815b4c2cbaeed5c7a6c138d8
Zalfapontianak <102153102+Zalfapontianak@users.noreply.github.com>
2022-06-03 20:43:04 +0700

    Update snk2.js

--- plugins/snk2.js
+++ plugins/snk2.js
@@ -34,7 +34,7 @@ let name = await conn.getName(m.sender)
 
 let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
 
-  sumberImg = await (await fetch(fla + 'RULES')).buffer()
+  sumberImg = await (await fetch(rules)).buffer()
   image = (await conn.prepareMessage('6285826114932@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
   res = await conn.prepareMessageFromContent(m.chat, {
     "productMessage": {
