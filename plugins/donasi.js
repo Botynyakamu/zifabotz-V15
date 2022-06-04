62fb7fbe6c2c88f9c885015da7fe79b2b5b17430
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-06-04 09:38:38 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -24,8 +24,9 @@ let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ?
         "description": '\n' + watermark + '\n' + str,
         "retailerId": "Thanks For Donate !",
         "url": "▌│█║▌║▌║║▌║▌║█│▌",
-        "descriptionCount": "999999999",
-        "productImageCount": "1",
+        "currencyCode": "IDR",
+        "priceAmount1000": "00000",
+"productImageCount": "1",
       },
       "businessOwnerJid": "6287773973818@s.whatsapp.net",
       "contextInfo": {
