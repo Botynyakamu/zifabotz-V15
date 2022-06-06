4726f6c206e22a18a7ebdd3cb85e5ef66702ba66
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-06-06 12:18:06 +0700

    Update and rename broadcasloc.js to broadcastloc.js

--- plugins/broadcastloc.js
+++ plugins/broadcastloc.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
     conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chats_\nestimation complete ${chats.length * 1.5} seconds`, m)
     for (let id of chats) {
         await delay(1500)
-        await conn.send2ButtonImg(id, logo,'*—「 Broadcast Zifabotz 」—*\n' + text, wm, '⋮☰ Menu', '.menu', 'Donasi', '.donasi')
+        await conn.send3ButtonImg(id, logo,'*—「 Broadcast Zifabotz 」—*\n' + text, wm, '⋮☰ Menu', '.menu', 'Donasi', '.donasi', 'Taati peraturan bot', '.rules', m)
     }
     m.reply('_*Broadcast Finished*_')
 }
