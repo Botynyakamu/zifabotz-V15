15ad71adbdd7f2b983fb1ee9ceb0f796b0a9a4c1
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-06-06 16:32:58 +0700

    Create _autovn.js

---
+++ plugins/_autovn.js
@@ -0,0 +1,15 @@
+let fs = require('fs')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn }) => {
+let lmafo = fs.readFileSync('./src/AUD-20200522-WA0283')
+let jembut = fs.readFileSync('./src/AUD-20200522-WA0283')
+conn.sendFile(m.chat, jembut, '', '', m, true)
+setTimeout(() => {
+conn.sendFile(m.chat, lmafo, '', '', m, true)
+}, 1000)
+}
+
+handler.customPrefix = /^(sc||.sc|script|sourcode|.sourcode|.script)$/i
+handler.command = new RegExp
+
+module.exports = handler
