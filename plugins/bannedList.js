b865201d83c879a9e14bc47dc32fb8e95c2836e8
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-06-06 09:32:53 +0700

    Update bannedList.js

--- plugins/bannedList.js
+++ plugins/bannedList.js
@@ -1,25 +1,30 @@
-let handler = async (m, { conn, isOwner }) => {
-    let chats = Object.entries(db.data.chats).filter(chat => chat[1].isBanned)
-    let users = Object.entries(db.data.users).filter(user => user[1].banned)
-    let caption = `
-┌〔 Daftar Chat Terbanned 〕
-├ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
-├ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
-├ ${isOwner ? '@' + jid.split`@`[0] : jid}
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let handler = async (m, { jid, conn, usedPrefix, command, isOwner }) => {
+let fetch = require('node-fetch')
+    let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
+    let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
+    let caption = `${readMore}
+╭─「 Chat Terbanned 」
+│✇ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
+│• ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : db.data.chats[jid].name}
+│• ${isOwner ? '@' + jid.split`-`[0] : jid}
 `.trim()).join('\n') : ''}
-└────
+╰────
 
-┌〔 Daftar Pengguna Terbanned 〕
-├ Total : ${users.length} Pengguna${users ? '\n' + users.map(([jid], i) => `
-├ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
-├ ${isOwner ? '@' + jid.split`@`[0] : jid}
+╭─「 Pengguna Terbanned 」
+│✇ Total : ${users.length} Pengguna${users ? '\n' + users.map(([jid], i) => `
+│• ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : db.data.users[jid].name}
+│• ${isOwner ? '@' + jid.split`@`[0] : jid}
 `.trim()).join('\n') : ''}
-└────
+╰────
 `.trim()
-    conn.reply(m.chat, caption, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } })
+    conn.send2ButtonImg(m.chat, await(await fetch(thumbfoto)).buffer(), caption, watermark, `Menu`, `${usedPrefix}menu`, `Owner`, `.owner`, m, { contextInfo: { mentionedJid: conn.parseMention(caption) }, mentions: await conn.parseMention(caption) })
 }
 handler.help = ['bannedlist']
 handler.tags = ['info']
-handler.command = /^listban(ned)?|ban(ned)?list|daftarban(ned)?$/i
+handler.command = /^list?ban(ned)?|ban(ned)?list?|daftarban(ned)?$/i
+
+handler.owner = false
 
 module.exports = handler
