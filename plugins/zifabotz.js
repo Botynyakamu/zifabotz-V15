e471cc3d2ca8293a2fca6178f9a836bf254272b8
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-13 16:04:14 +0700

    Update zifabotz.js

--- plugins/zifabotz.js
+++ plugins/zifabotz.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let caption = `*(￣ヘ￣）HEMM*`
 
-conn.send2Button( m.chat, caption, `©️ zifabotz`, `Menu`, `.?`, `donasi`, `.donasi`,  m)
+conn.send2Button( m.chat, caption, ``, `Menu`, `.?`, `donasi`, `.donasi`,  m)
 
        }
        
